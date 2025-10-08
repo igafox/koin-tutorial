76b92d909f88ea4aea6bc2d91f0fc551d8e252f3
igafox <49146536+igafox@users.noreply.github.com>
2025-10-08 17:39:14 +0900

    koin-core削除

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
-    implementation(libs.koin.core)
     implementation(libs.koin.android)
 
     testImplementation(libs.junit)
